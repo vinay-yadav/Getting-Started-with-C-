fc0b63c0480cbc913ab6331e8a2a756732da597e
Vinay <yadav.vinay1508@gmail.com>
2021-04-21 20:19:24 +0530

    Day 26 lower-upper bound

--- 01Basics/lower_upper_bound.cpp
+++ 01Basics/lower_upper_bound.cpp
@@ -1,22 +1,112 @@
-#include <iostream>     // std::cout
-#include <algorithm>    // std::lower_bound, std::upper_bound, std::sort
-#include <vector>       // std::vector
+/*
+lower_bound returns the element if exists for next greater element
+upper_bound return the next greater element
+
+for array and vector: sorting is must
+not for sets and maps
+
+Time Complexity: O(logn)
+Naive bounds for set and map uses O(n)
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int arr[] = {9,8,4,7,6,6,6,6,3,2,4};
-    vector<int> v(arr, arr+10);
-
-    sort(v.begin(), v.end());
-
-    for(int x: v) cout << x << " "; //2 3 4 4 5 5 6 7 8 9
-    cout << endl;
-
-    vector<int>::iterator low, up;
-    low = lower_bound (v.begin(), v.end(), 5); 
-    up = upper_bound (v.begin(), v.end(), 5);
-
-    cout << "lower_bound at position " << (low- v.begin()) << '\n';
-    cout << "upper_bound at position " << (up - v.begin()) << '\n';
-    return 0;
+void printArray(int arr[], int n){
+	for(int i = 0; i < n; ++i)
+		cout << arr[i] << " ";
+	cout << endl;
 }
+
+void printVector(vector<int> arr){
+	for(int x: arr)
+		cout << x << " ";
+	cout << endl;
+}
+
+// For Vector
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; ++i)
+    	cin >> arr[i];
+
+    sort(arr.begin(), arr.end());
+
+    printVector(arr);
+
+    auto it = lower_bound(arr.begin(), arr.end(), 26);
+    if(it == arr.end())
+    	cout << "Not found" << endl;
+    else
+    	cout << *it << endl;
+
+
+    it = upper_bound(arr.begin(), arr.end(), 8);
+    if(it == arr.end())
+    	cout << "Not found" << endl;
+    else
+    	cout << *it << endl;
+
+}
+
+
+/* For Set
+int main(){
+    int n;
+    cin >> n;
+
+    set<int> s;
+
+    while(n--){
+    	int x;
+    	cin >> x;
+    	s.insert(x);
+    }
+
+    auto it = s.lower_bound(7);
+    if(it == s.end())
+    	cout << "Not found" << endl;
+    else
+    	cout << *it << endl;
+
+    it = s.upper_bound(8);
+    if(it == s.end())
+    	cout << "Not found" << endl;
+    else
+    	cout << *it << endl;
+}
+*/
+
+/* For Array
+int main(){
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; ++i)
+    	cin >> arr[i];
+
+    sort(arr, arr + n);
+
+    printArray(arr, n);
+
+    int *ptr = lower_bound(arr, arr+n, 26);
+    if(ptr == arr+n)
+    	cout << "Not found" << endl;
+    else
+    	cout << *ptr << endl;
+
+
+    ptr = upper_bound(arr, arr+n, 8);
+    if(ptr == arr+n)
+    	cout << "Not found" << endl;
+    else
+    	cout << *ptr << endl;
+
+}
+*/
--- instant_run_setup/1.cpp
+++ instant_run_setup/1.cpp
@@ -3,5 +3,28 @@ using namespace std;
 
 
 int main(){
-	
+    int t;
+    cin >> t;
+
+    while(t--){
+        int k, m, n;
+        cin >> k >> m >> n;
+		// k = 3, m = 10, n = 2;
+        int steps = 0;
+
+        while(k != m){
+        	if(k < m){
+        		k = k * n;
+        		steps++;
+        	}else if(k - m >= 2){
+        		k = k - 2;
+        		steps;
+        	}else{
+        		k = k - 1;
+        		steps++;
+        	}
+        }
+
+        cout << steps << " " << k << endl;
+    }
 }
